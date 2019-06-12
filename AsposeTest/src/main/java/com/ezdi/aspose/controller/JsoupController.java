6dc74c2924409c70bed03e98dc490f8e1e29ec7d
Parth Patel <parth.patel@ezdi.us>
2019-06-12 16:36:48 +0530

    test for mvn release

--- AsposeTest/src/main/java/com/ezdi/aspose/controller/JsoupController.java
+++ AsposeTest/src/main/java/com/ezdi/aspose/controller/JsoupController.java
@@ -33,4 +33,50 @@ public class JsoupController {
 			}
 			System.out.println(doc.html());
 	}
+	
+	@GetMapping("addElement")
+	public void addElement(){
+		String html = "<div>\n" + 
+				"<p style=\"margin-top:0pt;  margin-bottom:0pt;\"><span>First line after start</span></p>\n" + 
+				"\n" + 
+				"<p style=\"margin-top:0pt;  margin-bottom:0pt;\"><span>&nbsp;</span></p>\n" + 
+				"\n" + 
+				"<p style=\"margin-top:0pt;  margin-bottom:0pt;\"><span>Hello there, how you doing, sounds good, okay then see you later.</span></p>\n" + 
+				"\n" + 
+				"<p style=\"margin-top:0pt;  margin-bottom:0pt;\"><span>&nbsp;</span></p>\n" + 
+				"\n" + 
+				"<p style=\"margin-top:0pt;  margin-bottom:0pt;\"><span>~!@#$%^&amp;*()_+|`1234567890-=\\</span></p>\n" + 
+				"\n" + 
+				"<p style=\"margin-top:0pt;  margin-bottom:0pt;\"><span>&nbsp;</span></p>\n" + 
+				"\n" + 
+				"<p style=\"margin-top:0pt;  margin-bottom:0pt;\"><span style=\"display:inline-block; width:36pt\">&nbsp;</span><span>Tab1</span><span style=\"display:inline-block; width:14.17pt\">&nbsp;</span><span style=\"display:inline-block; width:36pt\">&nbsp;</span><span>tab2</span></p>\n" + 
+				"\n" + 
+				"<p style=\"margin-top:0pt;  margin-bottom:0pt;\"><span>qwertyuiopasdfghjklzxcvbnm</span></p>\n" + 
+				"\n" + 
+				"<p style=\"margin-top:0pt;  margin-bottom:0pt;\"><span>QWERTYUIOPASDFGHJKLZXCVBNM</span></p>\n" + 
+				"\n" + 
+				"<p style=\"margin-top:0pt;  margin-bottom:0pt;\"><span>&nbsp;</span></p>\n" + 
+				"\n" + 
+				"<p style=\"margin-top:0pt;  margin-bottom:0pt;\"><span>[];&rsquo;./{}:&rdquo;&gt;?</span></p>\n" + 
+				"\n" + 
+				"<p style=\"margin-top:0pt;  margin-bottom:0pt;\"><span>&nbsp;</span></p>\n" + 
+				"\n" + 
+				"<p style=\"margin-top:0pt;  margin-bottom:0pt;\"><span>1234567890.+-*/</span></p>\n" + 
+				"\n" + 
+				"<p style=\"margin-top:0pt;  margin-bottom:0pt;\"><span>&nbsp;</span></p>\n" + 
+				"\n" + 
+				"<p style=\"margin-top:0pt;  margin-bottom:0pt;\"><span>Last line before end</span></p>\n" + 
+				"\n" + 
+				"<p style=\"margin-top:0pt;  margin-bottom:0pt;\"><span>&nbsp;</span></p>\n" + 
+				"</div>";
+		 org.jsoup.nodes.Document doc = Jsoup.parse(html);
+	        doc.outputSettings().syntax(org.jsoup.nodes.Document.OutputSettings.Syntax.xml);
+	        doc.outputSettings().charset("UTF-8");
+	        for (org.jsoup.nodes.Element element : doc.select("p > span")) {
+	        	if (element.attr("style").isEmpty() && element.text().trim().replace("\u00a0","").isEmpty()) {
+	        		 element.attr("style","-aw-import:ignore");
+	        	 }
+	        }
+	        System.out.println(doc.body().child(0).toString());
+	}
 }
--- AsposeTest/src/main/java/com/ezdi/aspose/controller/TemplateDownloadController.java
+++ AsposeTest/src/main/java/com/ezdi/aspose/controller/TemplateDownloadController.java
@@ -8,6 +8,7 @@ import com.aspose.words.DocumentBuilder;
 import com.aspose.words.NodeCollection;
 import com.aspose.words.NodeType;
 import com.aspose.words.Paragraph;
+import com.aspose.words.Run;
 import com.aspose.words.SaveFormat;
 import com.ezdi.aspose.Util;
 
@@ -25,12 +26,37 @@ public class TemplateDownloadController {
 		NodeCollection<Paragraph> paragraphs = doc.getChildNodes(NodeType.PARAGRAPH, true);
 
 		//methodOne(doc, start, paragraphs);
-		methodTwo(doc, start, paragraphs);
+		//methodTwo(doc, paragraphs);
+		methodThree(paragraphs);
 		doc.save(dataDir + "output.docx");
 		
 	}
 
-	private void methodTwo(Document doc, Paragraph start, NodeCollection<Paragraph> paragraphs) throws Exception {
+	private void methodThree(NodeCollection<Paragraph> paragraphs) throws Exception {
+		for (Paragraph para : paragraphs) {
+		    if (para.toString(SaveFormat.TEXT).startsWith("MED_BODY_CONTENT")) {
+		        if(para.getRuns().getCount() > 0)
+		        {
+		            Run run = (Run)para.getRuns().get(0).deepClone(true);
+		            run.setText("==START==");
+		            para.removeAllChildren();
+		            para.getRuns().add(run);
+
+		            Paragraph para2 =  (Paragraph) para.deepClone(true);
+		            para2.getRuns().get(0).setText("INSERT_HTML");
+		            para2 = (Paragraph) para.getParentNode().insertAfter(para2, para);
+
+		            Paragraph para3 =  (Paragraph) para.deepClone(true);
+		            para3.getRuns().get(0).setText("==END==");
+
+		            para2.getParentNode().insertAfter(para3, para2);
+		            break;
+		        }
+		    }
+		}
+	}
+
+	private void methodTwo(Document doc, NodeCollection<Paragraph> paragraphs) throws Exception {
 		for (Paragraph para : paragraphs) {
 		    if (para.toString(SaveFormat.TEXT).startsWith("MED_BODY_CONTENT")) {
 		        DocumentBuilder builder = new DocumentBuilder(doc);
